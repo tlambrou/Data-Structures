a3c9ecbbebb41e8ed202fccb9b4f7db357072fc6
tlambrou <lambrou@gmail.com>
2017-03-22 00:23:21 -0700

    Finished 1st homework assignment

--- source/bases.py
+++ source/bases.py
@@ -11,6 +11,18 @@ def decode(str_num, base):
     """
     assert 2 <= base <= 36
     # TODO: Decode number
+    result = 0
+    for digit in range(1, len(str_num) + 1):
+        value = str_num[-digit]
+        if ord(value) >= 97 and ord(value) <= 172:
+            value = int(ord(value) - 87)
+        elif ord(value) >= 65 and ord(value) <= 90:
+            value = int(ord(value) - 55)
+        else:
+            value = int(value)
+        placeholder = digit - 1
+        result = result + (value * base ** placeholder)
+    return result
 
 def encode(num, base):
     """
@@ -20,6 +32,45 @@ def encode(num, base):
     """
     assert 2 <= base <= 36
     # TODO: Encode number
+    num_rep={10:'a',
+         11:'b',
+         12:'c',
+         13:'d',
+         14:'e',
+         15:'f',
+         16:'g',
+         17:'h',
+         18:'i',
+         19:'j',
+         20:'k',
+         21:'l',
+         22:'m',
+         23:'n',
+         24:'o',
+         25:'p',
+         26:'q',
+         27:'r',
+         28:'s',
+         29:'t',
+         30:'u',
+         31:'v',
+         32:'w',
+         33:'x',
+         34:'y',
+         35:'z'}
+    result = ''
+    current = num
+    while current != 0:
+        remainder = current % base
+        if 36 > remainder > 9:
+            remainder_string = num_rep[remainder]
+        elif remainder >= 36:
+            remainder_string = '(' + str(remainder) + ')'
+        else:
+            remainder_string = str(remainder)
+        result = remainder_string + result
+        current = current / base
+    return result
 
 def convert(str_num, base1, base2):
     """
@@ -28,6 +79,8 @@ def convert(str_num, base1, base2):
     assert 2 <= base1 <= 36
     assert 2 <= base2 <= 36
     # TODO: Convert number
+    in_base_ten = decode(str_num, base1)
+    return encode(in_base_ten, base2)
 
 
 def main():
