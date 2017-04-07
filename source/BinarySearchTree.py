c627c2a590cd14883955feb7c884d20e3f2b4bb0
tlambrou <lambrou@gmail.com>
2017-04-07 00:59:26 -0700

    Completed Class 8 Assignment without unit tests

---
+++ source/BinarySearchTree.py
@@ -0,0 +1,294 @@
+#!python
+
+class BinaryNode(object):
+
+    def __init__(self, data=None):
+        """Initialize this node with the given data"""
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def __repr__(self):
+        """Return a string representation of this node"""
+        return 'Node({})'.format(repr(self.data))
+
+    def is_leaf(self):
+        """Check if the node is a leaf (has no children)"""
+        if self.left is not None:
+            return False
+        if self.right is not None:
+            return False
+        return True
+
+    def is_internal(self):
+        """Check if the node is internal (has at least one child)"""
+        return !self.is_leaf()
+
+    def is_single_baby_daddy(self):
+        """Returns True if node has one child and returns False otherwise"""
+
+    def is_double_baby_daddy(self):
+        """Returns True if node has two children and returns False otherwise"""
+        if self.left is None:
+            return False
+        if self.right is None:
+            return False
+        return True
+
+class BinarySearchTree(object):
+
+    def __init__(self, iterable=None):
+        """Initialize this binary search tree; append the given items, if any"""
+        self.root = None
+        self.size = 0
+        if iterable:
+            for item in iterable:
+                self.append(item)
+
+    def is_empty(self):
+        """"Return True if binary search tree is empty, False if not."""
+        if self.root is None:
+            return True
+        else:
+            return False
+
+    def insert(self, data):
+        """Insert a new node with data in order in the tree"""
+        if self.is_empty():
+            self.root.data = data
+            return
+        current = self.root
+        inserted = False
+        while inserted is False:
+            if data > current.data:
+                if current.right is None:
+                    current.right = BinaryNode(data)
+                    inserted = True
+                else:
+                    current = current.right
+            elif data < current.data:
+                if current.left is None:
+                    current.left = BinaryNode(data)
+                    inserted = True
+                else:
+                    current = current.left
+
+    def search(self, data):
+        """Check if a node with data is present in the tree"""
+        if self.is_empty():
+            return False
+        current = self.root
+        while current.is_leaf() is False:
+            if current.data == data:
+                return True
+            else:
+                if data > current.data:
+                    current = current.right
+                elif data < current.data:
+                    current = current.left
+        if current.data == data:
+            return True
+        else:
+            return False
+
+    def delete(self, data):
+        """Remove the node with data from the tree"""
+        # If the tree is empty exit
+        if self.is_empty():
+            return
+        # Set the current at the root
+        current = self.root
+        # Find the target node using find_node() method
+        target = self.find_node(data)
+        # 1st case: target node has no children (is a leaf)
+        if target.is_leaf():
+            # If the target is also the root...
+            if target is self.root:
+                # Remove the root and return
+                self.root = None
+                return
+            # Otherwise...
+            else:
+                # Find the parent node
+                parent = self.find_parent_node(target)
+                # Find which edge is the target
+                if parent.right is target:
+                    # Delete the node on the right and return
+                    parent.right = None
+                    return
+                elif parent.left is target:
+                    # Delete the node on the left and return
+                    parent.left = None
+                    return
+                else:
+                    raise ValueError('Something went wrong when finding the target and its parent')
+        # 2nd case: target node has 1 child
+        elif target.is_single_baby_daddy()):
+            # If the target is also the root...
+            if target is self.root:
+                # Find which child exists and make it the new root
+                if target.right is None:
+                    self.root = target.left
+                elif target.left is None:
+                    self.root = target.right
+            else:
+                # Otherwise find the parent node
+                parent = self.find_parent_node(target)
+                # If the target is to the right of the parent
+                if parent.right is target:
+                    # Find the target's child
+                    if target.left is None:
+                        # Point the parent to the target's child
+                        parent.right = target.right
+                        return
+                    elif target.right is None:
+                        # Point the parent to the target's child
+                        parent.right = target.left
+                        return
+                # If the target is to the left of the parent
+                elif parent.left is target:
+                    # Find the target's child
+                    if target.left is None:
+                        # Point the parent to the target's child
+                        parent.left = target.right
+                        return
+                    elif target.right is None:
+                        # Point the parent to the target's child
+                        parent.left = target.left
+                        return
+        # 3rd Case: target has 2 children
+        elif target.is_double_baby_daddy():
+            # If the target is the root
+            if target is self.root:
+                # Find in-order predecessor of target
+                iop = self.find_rightiest_node(target.left)
+                # Find in-order predecessor parent
+                iop_parent = self.find_parent_node(iop)
+                # If the IOP is the left branch of the target/root
+                if iop is target.left:
+                    # Set the right branch to point to the same node as the root
+                    iop.right = target.right
+                    # Promote IOP to the root
+                    self.root = iop
+                # Otherwise, if the IOP must be a right child of the target.left
+                else:
+                    # Point the IOP parent's right edge to the IOP's left node
+                    iop_parent.right = iop.left
+                    # Give the IOP the target's pointers
+                    iop.left = target.left
+                    iop.right = target.right
+                    # Finally make the IOP the tree's root
+                    self.root = iop
+                    return
+            # Otherwise (target is NOT the root)
+            else:
+                # Find the parent to the target
+                parent = self.find_parent_node(target)
+                # Find the IOP to the target
+                iop = self.find_rightiest_node(target.left)
+                # If the IOP is the target's left branch...
+                if target.left is iop:
+                    # Set the iop's right to the target's right branch
+                    iop.right = target.right
+                    # Determine which branch of the parent is pointing to the target
+                    if parent.right is target:
+                        # If right point it to the iop
+                        parent.right = iop
+                    elif parent.left is target:
+                        # If left point it to the iop
+                        parent.left = iop
+                else:
+                    # Find the IOP's parent
+                    iop_parent = self.find_parent_node(iop)
+                    # Point the IOP parent's right edge to the IOP's left node
+                    iop_parent.right = iop.left
+                    # Give the IOP the branch pointers of the target
+                    iop.right = target.right
+                    iop.left = target.left
+                    # Determine which branch the target is to the parent and set it to the iop
+                    if parent.right is target:
+                        parent.right = iop
+                    elif parent.left is target:
+                        parent.left = iop
+                    return
+
+
+    def find_node(self, data):
+        """Check if a node has the data and return the node"""
+        if self.is_empty():
+            return None
+        current = self.root
+        while current.is_leaf() is False:
+            if current.data == data:
+                return current
+            else:
+                if data > current.data:
+                    current = current.right
+                elif data < current.data:
+                    current = current.left
+        if current.data == data:
+            return current
+        else:
+            return None
+
+    def find_parent_node(self, target):
+        if self.is_empty():
+            return None
+        current = self.root
+        if current is target:
+            raise ValueError('Data found in root node')
+        while current.is_leaf() is False:
+            if target.data > current.data:
+                if current.right is target:
+                    return current
+                else:
+                    current = current.right
+            elif target.data < current.data:
+                if current.left is target:
+                    return current
+                else:
+                    current = current.left
+        return None
+
+    def find_rightiest_node(self, node):
+        """Given a node, find descendant node most to the right of it"""
+        current = node
+        while current.right != None:
+            current = current.right
+        return current
+
+
+def test_binary_search_tree():
+    bst = BinarySearchTree()
+    print(bst)
+
+    print('Appending items:')
+    bst.append('A')
+    print(bst)
+    bst.append('B')
+    print(bst)
+    bst.append('C')
+    print(bst)
+    print('head: ' + str(bst.root))
+    print('size: ' + str(bst.size))
+    print('length: ' + str(bst.length()))
+
+    print('Getting items by index:')
+    for index in range(bst.size):
+        item = bst.get_at_index(index)
+        print('get_at_index({}): {}'.format(index, repr(item)))
+
+    print('Deleting items:')
+    bst.delete('B')
+    print(bst)
+    bst.delete('C')
+    print(bst)
+    bst.delete('A')
+    print(bst)
+    print('head: ' + str(bst.root))
+    print('size: ' + str(bst.size))
+    print('length: ' + str(bst.length()))
+
+
+if __name__ == '__main__':
+    test_binary_search_tree()
